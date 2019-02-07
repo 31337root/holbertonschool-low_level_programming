6c3c22b4416d4afa134329e7381eafadec89d43f
31337root <[omarma@oulook.es]>
2019-02-07 01:31:22 +0000

    Fixing errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle")
-	puts()
-	retur(0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
