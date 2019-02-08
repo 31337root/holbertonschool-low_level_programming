32e351d06c0fda7e6688d2998b9cfec6f00eb7a8
31337root <[omarma@oulook.es]>
2019-02-08 00:54:57 +0000

    Fixing errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,16 +11,18 @@
 int main(void)
 {
 	int n;
-
+	int num;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
+	num = n % 10;
+	if (num > 5)
 	{
 	printf("The las digit of %d and is greater than 5\n", n);
 	}
 	else
 	{
-		if (n < 6 && n != 0)
+		if (num < 6 && n != 0)
 		{
 		printf("The las digit of %d and is less than 6 and not 0\n", n);
 		}
