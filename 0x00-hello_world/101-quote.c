6c6b8ed9c209dc596c0d1316cd8028c156ef2e2d
31337root <[omarma@oulook.es]>
2019-02-08 19:31:16 +0000

    Fixing errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	printf("\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "\n");
 	return (1);
 }
