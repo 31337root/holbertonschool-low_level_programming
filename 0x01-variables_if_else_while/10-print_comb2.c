26fef4f7fad4e6bbb59308d2921eb2e6a0e23a36
31337root <[omarma@oulook.es]>
2019-02-10 04:16:03 +0000

    Adding task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(48);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(49);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(50);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(51);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(52);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(53);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(54);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(55);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(56);
+		putchar(num);
+		putchar(44);
+		putchar(' ');
+	}
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(57);
+		putchar(num);
+		if (num != 57)
+			{
+				putchar(44);
+				putchar(' ');
+			}
+	}
+	putchar('\n');
+	return (0);
+}
