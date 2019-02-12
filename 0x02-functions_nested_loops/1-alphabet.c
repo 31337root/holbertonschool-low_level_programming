67d4b1eac0187bd54ed5a2af2d9b8088ea771bee
31337root <[omarma@oulook.es]>
2019-02-12 16:01:08 +0000

    Adding task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alp;
+	for (alp = 'a'; alp <= z; alp++)
+	{
+		_putchar(alp);
+	}
+	_putchar('\n');
+	return (0);
+}
