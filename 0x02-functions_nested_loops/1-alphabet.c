5c04a7c0f78451237a5d7ebc28ef2430d6715925
31337root <[omarma@oulook.es]>
2019-02-12 17:19:02 +0000

    Fixing errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,9 @@
 void print_alphabet(void)
 {
 	int alp;
-        for (alp = 'a'; alp <= 'z'; alp++)
-        {
-                _putchar(alp);
-        }
-        _putchar('\n');
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+	_putchar(alp);
+	}
+	_putchar('\n');
 }
