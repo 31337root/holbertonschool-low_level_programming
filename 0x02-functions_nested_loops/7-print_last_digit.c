d5168bbe53934fb34006a10e2322b1f3a86fc932
31337root <[omarma@oulook.es]>
2019-02-12 22:29:06 +0000

    Fixing errors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,14 @@ int print_last_digit(int num)
 	if (num > 0)
 	{
 		num = num % 10;
-		_putchar(num+'0');
+		_putchar(num + '0');
 		return (num);
 	}
 	else
 	{
-		num = num *-1;
+		num = num * -1;
 		num = num % 10;
-		_putchar(num+'0');
+		_putchar(num + '0');
 		return (num);
 	}
 }
