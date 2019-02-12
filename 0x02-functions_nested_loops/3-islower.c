7d4d4495edadb7a840419a0b0ed6bf16295f7e06
31337root <[omarma@oulook.es]>
2019-02-12 20:22:58 +0000

    Fixing errors

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
- * _islower - determinate if the input is lower or upper case
- * @c: value to process
- * Return: On lower case 1, otherwise 0.
+ *_islower - determinate if the input is lower or upper case
+ *@c: value to process
+ *Return: On lower case 1, otherwise 0.
 */
 
 int _islower(int c)
 {
 
-	if (c >= 97 || c <= 122)
+	if (c >= 97 && c <= 122)
 	{
-		_putchar(49);
+		return (1);
 	}
 	else
 	{
-		_putcher(48);
+		return (0);
 	}
 }
