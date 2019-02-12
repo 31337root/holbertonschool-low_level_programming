60faa1b0275678ade63afa7a2d728225c56776cf
31337root <[omarma@oulook.es]>
2019-02-12 20:06:58 +0000

    Adding task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _islower - determinate if the input is lower or upper case
+ * @c: value to process
+ * Return: On lower case 1, otherwise 0.
+*/
+
+int _islower(int c)
+{
+	int let;
+
+	if (let >= 97 || let <= 122)
+	{
+		_putchar(49);
+	}
+	else
+	{
+		_putcher(48);
+	}
+}
