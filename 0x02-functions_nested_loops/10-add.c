c2ace5e393719b9caba5ccfdaf4754b33d4bcc78
31337root <[omarma@oulook.es]>
2019-02-13 00:08:49 +0000

    Adding comments

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,12 @@
 #include "holberton.h"
-
+ /**
+ * add - Make de adition of two numbers
+ * @a,b: The numbers to be added
+ * Return: The result of the adition
+*/
 int add(int a, int b)
 	{
 		int c = a + b;
+
 		return (c);
 	}
