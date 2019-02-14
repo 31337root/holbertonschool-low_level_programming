52a59c1fab7def144ea10a706df738416d3ab313
31337root <[omarma@oulook.es]>
2019-02-14 19:48:24 +0000

    Adding tasks and changes to holberton.h

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 
-/*
- * _isupper - Function that return 1 if upper, 0 else
- *
+/**
+ * _isupper - Function that return 1 if upper, otherwise 0.
+ * Description: If c is between A and Z return 1 otherwise 0.
  * @c: Variable to be processed
+ * Return: integer.
 */
 
 int _isupper(int c)
 {
-	if (c > 65 || c < 90)
+	if (c > 65 && c < 90)
 	{
 		return (1);
 	}
