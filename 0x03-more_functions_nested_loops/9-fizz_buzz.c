b6f7eafd8471a490adc60a53281e98dead21b443
31337root <[omarma@oulook.es]>
2019-02-14 23:39:43 +0000

    Adding task 9 and function to holberton.h

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 101; n++)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		if ((n % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		if ((n % 5) != 0 && (n % 3) != 0)
+		{
+			printf("%d", n);
+		}
+	printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
