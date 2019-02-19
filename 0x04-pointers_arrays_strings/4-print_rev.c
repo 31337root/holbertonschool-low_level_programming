4d2725961051d1486191561da97dc1cb75f8ee29
31337root <[omarma@oulook.es]>
2019-02-19 23:56:29 +0000

    Adding task 4 and functions to holberton.h

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Function to print in reverse a string.
+ * Description: Reverse the characters of the string *s.
+ * @s: Argument to be processed.
+ * Return: void.
+*/
+
+void print_rev(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; ++l)
+	{}
+	for (; s[l] >= 0; l--)
+	{
+		_putchar(s[l]);
+	}
+	_putchar('\n');
+}
