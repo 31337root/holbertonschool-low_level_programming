ebe98b0ff5f07f51ee93650dc80fea5f6750a030
31337root <[omarma@oulook.es]>
2019-02-21 22:31:49 +0000

    Adding comments

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 
 /**
- * *_strncpy - 
- * Description: 
- *
- *
+ * *_strncpy - Function that copies a string.
+ * Description: Function to copy the n characters of src to dest.
+ * @src: String to be copied.
+ * @dest: String to be modified.
+ * @n: The number of characters to be copied.
+ * Return: char.
 */
 
 char *_strncpy(char *dest, char *src, int n)
