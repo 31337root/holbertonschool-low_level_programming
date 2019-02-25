f4117c501fcf9926bb8da03a93d0436bfdb34813
31337root <[omarma@oulook.es]>
2019-02-25 22:32:17 +0000

    Adding task 2 and new funtions to holberton.h

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - Function that locates a character in a string.
+ * Description: Returns a pointer to the first occurrence of the
+ * character c in the string s, or NULL if the character is not found.
+ * @s: String to be analized.
+ * @c: Character to be found.
+ * Return: char.
+*/
+
+char *_strchr(char *s, char c)
+{
+	char *location;
+	int count;
+
+	for (count = 0; s[count] != c; count++)
+	{}
+
+	if (s[count] == c)
+	{
+		location = &s[count];
+	}
+	else
+	{
+		location = "NULL";
+	}
+	return (location);
+}
