868bce61a5540cf256153fad0494f70ab9614974
31337root <[omarma@oulook.es]>
2019-03-04 00:00:40 +0000

    Adding task 1

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: The number of command line arguments.
+ * @argv: An awway containing the program command line arguments.
+ * Return: Success Always (0).
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x09-argc_argv/README.md
@@ -0,0 +1,3 @@
+# Tasks requested by Holberton School about
+# 0x09. C - argc, argv
+# 0-whatsmyname.c, program that prints its name, followed by a new line.
