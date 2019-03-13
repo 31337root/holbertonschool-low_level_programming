9e731ed526273fde18668af2526feffb91b9af50
31337root <[omarma@oulook.es]>
2019-03-13 04:01:37 +0000

    Adding task 0, README file and more

---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# All functions and programs requested by Holberton school.
+# 0x0D. C - Structures, typedef.
+# dog.h - new type struct dog with elements.
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - new type with elements.
+ * Description: Will store the name, age and owner of the dog.
+ * @name: Member 1.
+ * @age: Member 2.
+ * @owner: Member 3.
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* _DOG_H_ */
