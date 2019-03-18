cde4bb784376eaa4e2948308e4c98052124f4c6a
31337root <[omarma@oulook.es]>
2019-03-18 04:46:39 +0000

    Adding advanced task 4

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,14 @@
-section .data
-	msg db "Hello, Holberton",10
+          global    _start
 
-section .text
-	global _start
+          section   .text
+_start:   mov       rax, 1
+          mov       rdi, 1
+          mov       rsi, message
+          mov       rdx, 13
+          syscall
+          mov       rax, 60
+          xor       rdi, rdi
+          syscall
 
-_start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 13
-	syscall
-
-	mov rax, 60
-	mov rdi, 0
-	syscall
+          section   .data
+message:  db        "Hello, World", 10 
