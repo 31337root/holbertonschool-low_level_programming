0c1f0a1c8262d62fa8d77a3f8429ad62b3271998
31337root <omarma@outlook.es>
2019-03-21 23:56:40 +0000

    Adding description to function task 0

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
- *
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: Pointer structure to recieve.
+ * Return: size_t.
 */
 
 size_t print_list(const list_t *h)
@@ -14,7 +12,8 @@ size_t print_list(const list_t *h)
 	int i = 0;
 
 	while (h != NULL)
-	{	if (h->str != NULL)
+	{
+		if (h->str != NULL)
 		{
 			printf("[%u] %s\n", h->len, h->str);
 		}
