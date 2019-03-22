9f58aa9cbb9fba4a0e93359bff05877f963414e9
31337root <omarma@outlook.es>
2019-03-22 04:29:33 +0000

    Fixing errors

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -47,7 +47,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		for (temp = *head; temp->next != NULL; temp = temp->next)
 		{}
+		temp->next = new_node;
 	}
-	temp->next = new_node;
 	return (new_node);
 }
