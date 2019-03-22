b2fac95daee4caba92d42b553873e0d83c7bf395
31337root <omarma@outlook.es>
2019-03-22 01:11:59 +0000

    Adding task 2

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * _strlen - Function to count the length of a string.
+ * Description: Will count the number of characters from the input.
+ * @s: Parameter to be processed
+ * Return: Integer.
+*/
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; ++c)
+	{}
+	return (c);
+}
+
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list.
+ * @head: ponter structure to recieve.
+ * @str: String to recieve.
+ * Return: The adress of the new node.
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node  = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = _strlen(new_node->str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
