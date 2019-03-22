7a43e6058455d0e729e27aef67754fd7767e1c2d
31337root <omarma@outlook.es>
2019-03-22 04:18:02 +0000

    Adding task 3

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * _strlen - Function to count the length of a string.
+ * Description: Will count the number of characters from the input.
+ * @s: Parameter to be processed
+ * Return: Integer.
+*/
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; ++c)
+	{}
+	return (c);
+}
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+
+	new_node  = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(new_node->str);
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		for (temp = *head; temp->next != NULL; temp = temp->next)
+		{}
+	}
+	temp->next = new_node;
+	return (new_node);
+}
