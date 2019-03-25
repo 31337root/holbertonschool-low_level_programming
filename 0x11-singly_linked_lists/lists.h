d7ed5df3182a20751f20d4b16c3108d7546c2fc1
31337root <omarma@outlook.es>
2019-03-25 12:59:43 +0000

    Adding prototype to header file

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,6 +13,7 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct list_s
 {
 	char *str;
@@ -25,5 +27,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int _strlen(char *s);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* _LIST_H_ */
