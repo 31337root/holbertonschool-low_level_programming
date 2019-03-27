6aabac883d1eb7cfaf9091312b7e48d8b6164f71
31337root <omarma@outlook.es>
2019-03-27 03:26:43 +0000

    Adding task 5

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: List to be freed.
+ * Return: void.
+*/
+
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+	{
+		return;
+	}
+	free_listint2((&(*head)->next));
+	free(*head);
+}
