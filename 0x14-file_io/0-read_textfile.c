a4c02ca2f74f7cff19ca79c951289754248053f1
31337root <omarma@outlook.es>
2019-04-04 01:18:18 +0000

    Adding task 0, README.md and holberton.h

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - function that reads a text file and prints it
+ * to the POSIX standard output.
+ * @filename: Name of the file to be read and print
+ * @letters: letters is the number of letters it should read and print.
+ * Return: int/ssize_t - IF SUCCESS: Nuber of printed characters,
+ * IF FAILURE: 0.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, wr, rd;
+	char *buf;
+
+	if (filename)
+	{
+		buf = malloc(sizeof(buf) * letters);
+		if (buf)
+		{
+			fd = open(filename, O_RDONLY, 0700);
+			if (fd == -1)
+				return (0);
+			rd = read(fd, buf, letters);
+			if (rd == -1)
+				return (0);
+			wr = write(1, buf, rd);
+			if (wr == -1)
+				return (0);
+			return (wr);
+		}
+	}
+	return (0);
+}
---
+++ 0x14-file_io/README.md
@@ -0,0 +1,4 @@
+# All functions and programs requested by Holberton school.
+# 0x14. C - File I/O
+# holberton.h - Header files for Holberton tasks.
+# 0-read_textfile.c -  function that reads a text file and prints it to the POSIX standard output.
