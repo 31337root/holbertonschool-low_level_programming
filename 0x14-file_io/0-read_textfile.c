1834dabd0b2654581f05f4784fca3146db10fb38
31337root <omarma@outlook.es>
2019-04-04 01:21:13 +0000

    Adding a conditional to write evaluator

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			if (rd == -1)
 				return (0);
 			wr = write(1, buf, rd);
-			if (wr == -1)
+			if (wr == -1 || wr != rd)
 				return (0);
 			return (wr);
 		}
