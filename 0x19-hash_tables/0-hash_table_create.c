5d4e68db65e49e8b8dca33428ad4cb53ee880985
31337root <omarma@outlook.es>
2019-05-31 22:58:54 +0000

    Adding size case

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,13 +10,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	new_table = malloc(sizeof(hash_table_t));
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table && new_table->array)
+	if (size)
 	{
-		new_table->size	= size;
-		return (new_table);
+		new_table = malloc(sizeof(hash_table_t));
+		new_table->array = malloc(sizeof(hash_node_t *) * size);
+		if (new_table && new_table->array)
+		{
+			new_table->size	= size;
+			return (new_table);
+		}
 	}
-	else
-		return (NULL);
+	return (NULL);
 }
