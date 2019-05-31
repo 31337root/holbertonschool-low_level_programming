f3001afd1001ff5709848b52bb95ffeccaf24906
31337root <omarma@outlook.es>
2019-05-31 23:24:02 +0000

    Fixing Betty

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new_table = malloc(sizeof(hash_table_t));
 		if (new_table)
 		{
-	                new_table->array = malloc(sizeof(hash_node_t *) * size);
+			new_table->array = malloc(sizeof(hash_node_t *) * size);
 			if (new_table->array)
 			{
 				new_table->size	= size;
