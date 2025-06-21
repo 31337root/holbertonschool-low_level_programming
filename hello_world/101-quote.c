28187e91dae71ba8621b3516ead13859ea5c934f
root0 <root0@192.168.1.6>
2025-06-21 11:27:15 -0500

    2nd Attempt exercise 9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
 */
 
 int main(void)
-
 {
-	write(2, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19");
-	write(2, "\n");
+	char error_message[61] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, error_message, strlen(error_message));
 	return (1);
 }
