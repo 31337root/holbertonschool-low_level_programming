2973e04284e2801b6bbf3407973735eb27490dda
root0 <root0@192.168.1.6>
2025-06-22 22:55:34 -0500

    Adding exercise 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - The function make de adition of two numbers
+ * @a: Number 1 to be added
+ * @b: Number 2 to be added
+ * Return: The result of the adition
+ *
+*/
+
+int add(int a, int b)
+	{
+		int c = a + b;
+
+		return (c);
+	}
