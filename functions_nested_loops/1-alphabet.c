641fd3667e3f33d4119bf6c4cddf35215b1667b5
root0 <root0@192.168.1.6>
2025-06-22 22:37:06 -0500

    Adding exercise 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	int alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+	_putchar(alp);
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 /* Header file for Holberton project functions, nested loops */
 
 int _putchar(char c);
+void print_alphabet(void);
+
