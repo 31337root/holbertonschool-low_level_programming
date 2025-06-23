9681db95dbe086638f3bbcccddfbf03960d352f5
root0 <root0@192.168.1.6>
2025-06-22 23:31:39 -0500

    12th attempt exercise 14

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -5,19 +5,22 @@
 int main(void)
 
 {
+
 	long a, b, c;
 	a = 1;
 	b = 2;
-	
+
 	printf("%ld, ", a);
 	printf("%ld, ", b);
 
 	while (c < 12000000000)
 	{
+
 		c = a + b;
 		a = b;
 		printf("%ld, ", c);
 		b = c;
+
 	}
 	c = a + b;
 	printf("%ld\n", c);
