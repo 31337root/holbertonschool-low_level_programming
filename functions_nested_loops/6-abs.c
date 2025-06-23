3a913eae6c01667df887d14c7ea4f2b57656ce6d
root0 <root0@192.168.1.6>
2025-06-22 22:46:25 -0500

    Adding exercise 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Compute the absolute value of an integer
+ * @num: value to process
+ * Return: the value indicted
+*/
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		num = num * -1;
+		return (num);
+	}
+	else
+	{
+		return (num);
+	}
+}
