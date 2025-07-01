d5d3010afd2b06aa6975b6883b8865e2dc0206d0
root0 <root0@192.168.1.6>
2025-07-01 18:42:30 -0500

    Adding exercise 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function to change the value to a parameter.
+ * Description: Change the value of a parameter to 98.
+ * @n: Variable to be processed.
+ * Return: void.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+/* Header files for Holberton tasks (0x04. C - Pointers, arrays 
+ * and strings)
+*/
+
+void reset_to_98(int *n);
