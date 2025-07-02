9b93704cce1b67774a9f95facf5b43a72ef834ce
root0 <root0@192.168.1.6>
2025-07-01 20:20:30 -0500

    Adding exercise 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Function to print certain number of integers of an array
+ * @*a: Pointer to array to be printed
+ * @n: Length to be printed
+ * Return: void.
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+	n = n - 2;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[i]);
+}
