0bf861d47b9c9b8b0a4a06f905ad226e56549d1b
root0 <root0@192.168.1.6>
2025-07-02 18:52:56 -0500

    Adding exercise 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *cap_string - Function to capitalizes two strings
+ * Description: Return a processed or not
+ * @a: String to be capitalized
+ * Return: char
+*/
+
+char *cap_string(char *a)
+{
+	if (a)
+	{
+		int i;
+
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			if (!((a[i] >= 'a' && a[i] <= 'z') || \
+				(a[i] >= 'A' && a[i] <= 'Z')))
+			{
+				while (a[i] != '\0')
+				{
+					i++;
+					if (a[i] >= 'a' && a[i] <= 'z')
+					{
+						a[i] = a[i] - 32;
+						break;
+					}
+				}
+			}
+		}
+		return (a);
+	}
+	return (a);
+}
