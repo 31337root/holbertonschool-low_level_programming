e1f67c832fd46a619a2250fb7f31b41df02ab17c
root0 <root0@192.168.1.6>
2025-07-02 15:10:01 -0500

    2nd attempt exercise 4-1

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 
 /**
- * *_strncat - Function to concatenate two strings.
- * Description: Concatenate the number of characters given on n.
- * @dest: String to be concatenate.
- * @src: String to be copied.
- * @n: Numer of characters to be copied.
- * Return: char.
+ * reverse_array - Function to reverses the content of an array of integers.
+ * @a: Array to be reversed.
+ * @n: Number of items on array.
+ * Return: void
 */
 
 void reverse_array(int *a, int n)
@@ -16,6 +14,7 @@ void reverse_array(int *a, int n)
 	{
 		int *temp = (int *)malloc(n * sizeof(int));
 		int i, ii;
+
 		ii = 0;
 
 		for (i = n - 1; i >= 0; i--)
@@ -24,11 +23,12 @@ void reverse_array(int *a, int n)
 			ii++;
 		}
 		ii = 0;
+
 		for (i = n - 1; i >= 0; i--)
-                {
-                        a[ii] = temp[ii];
-                        ii++;
-                }
+		{
+			a[ii] = temp[ii];
+			ii++;
+		}
 	}
-	
+
 }
